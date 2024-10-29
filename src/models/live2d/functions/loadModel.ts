399cb4fb4bf79d656a76b58a3f9cacbe701d7178
zoollcar <zoollcar@qq.com>
2024-10-29 12:55:33 +0800

    remove model haru

--- src/models/live2d/functions/loadModel.ts
+++ src/models/live2d/functions/loadModel.ts
@@ -3,7 +3,6 @@ import { Live2DModel } from "pixi-live2d-display-lipsyncpatch";
 
 // load model
 export default async function loadModel(
-  // modelName = "./assets/haru/haru_greeter_t03.model3.json"
   modelName = "./assets/IceGIrl/icegirl.model3.json"
 ) {
   return await Live2DModel.from(modelName, {
